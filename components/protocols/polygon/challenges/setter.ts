6b1192cff32056d2c2fc5d5936f1cee094fa185b
akshayveer <akshayveer096@gmail.com>
2022-05-03 15:36:03 +0530

    set value

--- components/protocols/polygon/challenges/setter.ts
+++ components/protocols/polygon/challenges/setter.ts
@@ -10,9 +10,13 @@ const setValue = async (contractAddress: string, value: number) => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     // try to figure out the expected parameters
-    const contract = new ethers.Contract(undefined);
+    const contract = new ethers.Contract(
+      contractAddress,
+      SimpleStorageJson.abi,
+      signer,
+    );
     // try to figure out the expected method
-    const transactionResult = undefined;
+    const transactionResult = await contract.set(value);
     const receipt = await transactionResult.wait();
     return {hash: receipt.transactionHash};
   } catch (error) {
